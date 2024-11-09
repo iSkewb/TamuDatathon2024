2a1c16b3b74ee1a58a64ec0448c95b4151db9e0b
LukeConran <145418221+LukeConran@users.noreply.github.com>
2024-11-09 11:53:07 -0600

    Processed Data
    Processed Data

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # Read in the data
-df = pd.read_csv("connections_game_data.csv")
+df = pd.read_csv("../data/Connections_Data.csv")
 
 # Drop the 'Starting Row' and 'Starting Column' columns
 df_cleaned = df.drop(columns=['Starting Row', 'Starting Column'])
 
 # Optionally, save the cleaned data
-df_cleaned.to_csv("connections_game_data_cleaned.csv", index=False)
+df_cleaned.to_csv("../data/data_cleaned.csv", index=False)
